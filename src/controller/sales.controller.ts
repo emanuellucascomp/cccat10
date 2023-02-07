4d9c7fa712fdc049eadeddd31cb98557456f91c8
emanuellucascomp <cardgameheritage@gmail.com>
2023-02-07 20:03:57 -0300

    adding validator

--- src/controller/sales.controller.ts
+++ src/controller/sales.controller.ts
@@ -1,8 +1,7 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import { createSaleForCustomer } from "../service/sales.service";
 
 export const createSale = (req: Request, res: Response) => {
-    console.log(req.body)
     const { document, items, coupon } = req.body;
     const createSaleResponse = createSaleForCustomer(document, items, coupon);
     res.json(createSaleResponse)
--- src/service/sales.service.ts
+++ src/service/sales.service.ts
@@ -1,6 +1,13 @@
+import { validate } from "../common/validator"
+
 export const createSaleForCustomer = (document: string, items: any[], coupon: number) => {
-    console.log(document)
-    console.log(items)
-    console.log(coupon)
-    return { message: "It worked" }
+    let total = 0
+    if(!validate(document)) throw new Error('CPF Inválido')
+    items.map(item => {
+        total += item.price * item.quantity
+    })
+
+    total -= (total * coupon) / 100
+
+    return { total }
 }
